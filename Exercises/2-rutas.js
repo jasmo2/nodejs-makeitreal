684af946563f05e98a0b69b8c76022ff981cb2b2
jaime <jaime.molina@mattersupply.co>
2020-02-15 18:37:06 -0500

    404 route

--- Exercises/2-rutas.js
+++ Exercises/2-rutas.js
@@ -4,17 +4,12 @@
  * -- 404 es conocido en los códigos porqué significa que
  * -- NO existe en nuestro programa-web
  *
-<<<<<<< HEAD
  * Crear los siguientes Tags que contengan la respuesta
  * <body>, <h1> no existe </h1>, <h2> 404 </h2>
  *
  * Para ejecutar su programa desde consola usar
  * `yarn ex:routes`
  *
-=======
- * Para ejecutar su programa desde consola usar
- * `yarn ex:routes`
->>>>>>> rutas
  */
 
 const express = require('express')
@@ -24,8 +19,20 @@ app.get('/', (req, res) => {
   res.send('<h1>Pagina Root Si existe</h1>')
 })
 
-<<<<<<< HEAD
-=======
+app.use(function(req, res) {
+  res.status(404)
+  res.send(`
+  <body style='
+    align-items: center;
+    display: flex; 
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+  '>
+   
+    <h1 style='text-align:center;'>Esta pagina no existe</h1>
+    <h2 style='text-align:center;'> 404 </h2>
+   </body>`)
+})
 
->>>>>>> rutas
 app.listen(9000, () => console.log('Listening on port 9000!'))
