2ad4685ed5c936bf2010c4db564598ea783a9a6f
jaime <jaime.molina@mattersupply.co>
2020-02-16 02:46:51 -0500

    exercise middleware

--- Exercises/4-middleware.js
+++ Exercises/4-middleware.js
@@ -9,6 +9,7 @@
 
 const logger = (req, res, next) => {
   //Añadir el código aquí
+  console.log('TCL: logger -> req.method', req.method)
 
   next() // esto es necesario para que la petición continúe
 }
@@ -17,5 +18,19 @@ const express = require('express')
 const app = express()
 
 app.use(logger)
+app.get('/', (req, res) => {
+  res.send('GET request')
+})
+app.post('/', (req, res) => {
+  res.send('POST request')
+})
+
+app.put('/', (req, res) => {
+  res.send('PUT request')
+})
+
+app.delete('/', (req, res) => {
+  res.send('DELETE request')
+})
 
 app.listen(9000, () => console.log('Listening on port 9000!'))
