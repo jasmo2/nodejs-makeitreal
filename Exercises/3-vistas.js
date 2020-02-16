5f534e42c305adfad3f8dd63c85714334c56cd2a
jaime <jaime.molina@mattersupply.co>
2020-02-15 19:09:22 -0500

    pug Exercise

---
+++ Exercises/3-vistas.js
@@ -0,0 +1,21 @@
+/**
+ *  El ejercicio constará de 3 cosas.
+ *  1º crear otra ruta `user-template` con un template que reciba
+ *  nombre como argumento.
+ *
+ *  2º crear un partial/footer con sus tags html
+ *
+ * 3º Adicionar el header existente en partials
+ * `yarn ex:templates`
+ */
+
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/user-template', (req, res) => {
+  res.render('user-template', { name: 'jasmo2' })
+})
+
+app.listen(9000, () => console.log('Listening on port 9000!'))
