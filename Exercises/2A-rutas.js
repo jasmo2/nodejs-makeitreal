e26889bd6f34a74e212d0dabc33ad0b823058b4a
jaime <jaime.molina@mattersupply.co>
2020-02-27 19:07:05 -0500

    2a resolution

--- Exercises/2A-rutas.js
+++ Exercises/2A-rutas.js
@@ -1,6 +1,6 @@
 /**
  * Para este ejercicio escribamos 5 rutas
- * - get que reciba en la raíz (root)
+ * - get que reciba en la raíz (/)
  * - get que reciba en (/custom)
  * - put que reciba en (/custom)
  * - post que reciba en (/custom)
@@ -15,5 +15,33 @@
 
 const express = require('express')
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Index Page')
+})
+
+app.get('/custom/', (req, res) => {
+  res.send('Get Page')
+})
+
+app.get('/custom/:userId/:name/', (req, res) => {
+  const { userId, name } = req.params
+  res.send(`Get Page II id: ${userId} name: ${name}`)
+})
+
+app.put('/custom/:userId', (req, res) => {
+  const { userId } = req.params
+  console.log('TCL: userId', userId)
+  res.send(`Put Page ${userId}`)
+})
+app.delete('/custom/:userId', (req, res) => {
+  const { userId } = req.params
+
+  console.log('TCL: userId', userId)
+  res.send(`Delete Page ${userId}`)
+})
+app.post('/custom', (req, res) => {
+  res.send('Post Page')
+})
 
 app.listen(9000, () => console.log('Listening on port 9000!'))
