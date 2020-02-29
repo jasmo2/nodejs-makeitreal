d598f98814c09cd9b1d223297feed3c40ca558c0
jaime <jaime.molina@mattersupply.co>
2020-02-28 19:29:18 -0500

    solve conflicts

--- Exercises/2B-rutas.js
+++ Exercises/2B-rutas.js
@@ -16,7 +16,47 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Pagina Root Si existe</h1>')
+  res.send('Index Page')
+})
+
+app.get('/custom/', (req, res) => {
+  res.send('Get Page')
+})
+
+app.get('/custom/:userId/:name/', (req, res) => {
+  const { userId, name } = req.params
+  res.send(`Get Page II id: ${userId} name: ${name}`)
+})
+
+app.put('/custom/:userId', (req, res) => {
+  const { userId } = req.params
+  console.log('TCL: userId', userId)
+  res.send(`Put Page ${userId}`)
+})
+app.delete('/custom/:userId', (req, res) => {
+  const { userId } = req.params
+
+  console.log('TCL: userId', userId)
+  res.send(`Delete Page ${userId}`)
+})
+app.post('/custom', (req, res) => {
+  res.send('Post Page')
+})
+
+app.use((req, res) => {
+  res.status(404)
+  res.send(`
+    <body style='
+      align-items: center;
+      display: flex; 
+      flex-direction: column;
+      height: 100vh;
+      justify-content: center;
+    '>
+      <h1 style='text-align:center;'>Esta pagina no existe</h1>
+      <h2 style='text-align:center;'> 404 </h2>
+    </body>
+   `)
 })
 
 app.listen(9000, () => console.log('Listening on port 9000!'))
--- Exercises/3-vistas.js
+++ Exercises/3-vistas.js
@@ -1,4 +1,5 @@
 /**
+ *
  *  El ejercicio constará de 3 cosas.
  *  1º crear otra ruta `user-template` con un template que reciba
  *  nombre como argumento.
@@ -6,8 +7,8 @@
  *  2º crear un partial/pieDePagina con sus tags html
  *
  * 3º Adicionar el header existente en partials
- * 
- * 
+ *
+ *
  * `yarn ex:templates`
  */
 
@@ -16,14 +17,11 @@ const app = express()
 
 app.set('view engine', 'pug')
 
-app.get("/",(req, res) => {
+app.get('/', (req, res) => {})
 
+app.get('/user-template/:nombre', (req, res) => {
+  const { nombre } = req.params
+  res.render('template', { name: nombre })
 })
-
-app.get("/user-template/:nombre",(req, res) => {
-    const {nombre} = req.params;
-    res.render('template', { name: nombre })
-})
-
 
 app.listen(9000, () => console.log('Listening on port 9000!'))
